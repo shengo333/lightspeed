f098b3ef12d4b859aa7143f3fd281ad41f91c155
beka shengelia <beka.shengelia@airteam.ai>
2025-09-10 14:22:06 +0400

    start vue app with dumy component

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TestComponent />
+    testcomponent2
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import TestComponent from './components/TestComponent.vue';
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="test-component">
+    <h1>Test Component</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.test-component { 
+    background: red;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+    margin: 20px;
+    h1{
+        color: white;
+        font-size: 20px;
+        font-weight: bold;
+        text-align: center;
+    }
+}
+
+
+</style>
